a9644717fca28550c11b54cfa00ebfb7458ae9c1
PrtmPhlp <p.pertermann@icloud.com>
2025-02-13 20:43:30 +0100

    fix: always display feature text above image on mobile & update copyright year (#11)

--- src/components/feature-box.tsx
+++ src/components/feature-box.tsx
@@ -11,16 +11,14 @@ export default function FeatureBox({
 }) {
   return (
     <div className="p-6 glass grid grid-cols-1 md:grid-cols-12 gap-10 items-center">
-      <div className="col-span-5">
+      <div className={`col-span-5 order-1 ${imgLeft ? "md:order-2" : "md:order-1"}`}>
         <p className="text-3xl font-semibold !mb-3">{title}</p>
         <p className="text-gray-300">{description}</p>
       </div>
       <img
         src={imgSrc}
         alt="Feature Image"
-        className={`rounded-lg col-span-7 border border-neutral-800 ${
-          imgLeft ? "order-first" : "order-last"
-        }`}
+        className={`rounded-lg col-span-7 border border-neutral-800 order-2 ${imgLeft ? "md:order-1" : "md:order-2"}`}
       />
     </div>
   );
